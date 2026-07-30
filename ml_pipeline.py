7beb95c70efdc596038a9ab008d5a8e9585a2783
andreagastaldi03 <andrea.gastaldi03@universitadipavia.it>
2026-07-30 11:29:01 +0200

    ampliamento grid

--- ml_pipeline.py
+++ ml_pipeline.py
@@ -70,8 +70,8 @@ def get_model_grid():
     ])
     # elastic net contiene both L1 e L2, C e l1 cercano un bilancio tra le due
     en_grid = {
-        "clf__C": [0.01, 0.05, 0.1, 0.5, 1.0],
-        "clf__l1_ratio": [0.1, 0.3, 0.5, 0.7, 0.9],
+        "clf__C": [0.001, 0.01, 0.05, 0.1, 0.5, 1.0],
+        "clf__l1_ratio": [0.05, 0.1, 0.3, 0.5, 0.7, 0.9],
     }
     models["elastic_net"] = (en_pipe, en_grid)
 
